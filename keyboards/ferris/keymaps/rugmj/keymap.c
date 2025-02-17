e3ef979ef65d880374aa9507451794b71d0793c0
rugmj <rugmj@rugmj.dev>
2025-02-17 17:56:49 +0000

    feat(keymap): make nav right thumb 2 will be able to hold backspace

--- keyboards/ferris/keymaps/rugmj/keymap.c
+++ keyboards/ferris/keymaps/rugmj/keymap.c
@@ -91,13 +91,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    ├───────────────┼─────┼─────┼─────┼─────┤   ├─────┼──────┼──────┼──────┼──────┤
 //    │               │     │     │     │     │   │     │      │ pgdn │ pgup │      │
 //    └───────────────┴─────┴─────┼─────┼─────┤   ├─────┼──────┼──────┴──────┴──────┘
-//                                │     │     │   │     │      │
+//                                │     │     │   │     │ bspc │
 //                                └─────┴─────┘   └─────┴──────┘
 [_NAV] = LAYOUT_split_3x5_2(
-  QK_LAYER_LOCK , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,     KC_TRNS , KC_TRNS , KC_TRNS   , KC_TRNS , KC_TRNS ,
-  KC_TRNS       , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,     KC_TRNS , KC_LEFT , KC_DOWN   , KC_UP   , KC_RIGHT,
+  QK_LAYER_LOCK , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,     KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
+  KC_TRNS       , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,     KC_TRNS , KC_LEFT , KC_DOWN , KC_UP   , KC_RIGHT,
   KC_TRNS       , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,     KC_TRNS , KC_TRNS , KC_PGDN , KC_PGUP , KC_TRNS ,
-                                      KC_TRNS , KC_TRNS ,     KC_TRNS , KC_TRNS
+                                      KC_TRNS , KC_TRNS ,     KC_TRNS , KC_BSPC
 ),
 
 //    ┌───────────────┬─────┬─────┬─────┬─────┐   ┌──────┬──────┬──────┬──────┬──────┐
